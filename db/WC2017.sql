5deb4bd55ae0ca8949e65c66975223d1bf78f809
Charuka Karunanayake <wiredPh4nt0m@atlassian.com>
2016-12-03 13:45:53 +0530

    sql file

---
+++ db/WC2017.sql
@@ -0,0 +1,124 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 03, 2016 at 08:33 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `WC2017`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Questions`
+--
+
+CREATE TABLE `Questions` (
+  `sessionID` varchar(6) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `votes` int(11) NOT NULL,
+  `question` varchar(140) NOT NULL,
+  `qID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Sessions`
+--
+
+CREATE TABLE `Sessions` (
+  `sessionID` varchar(6) NOT NULL,
+  `sessionName` varchar(30) NOT NULL,
+  `PresenterName` varchar(60) NOT NULL,
+  `PresenterQual` varchar(60) NOT NULL,
+  `sessionTime` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `username` varchar(30) NOT NULL,
+  `password` varchar(21) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `lname` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userfav`
+--
+
+CREATE TABLE `userfav` (
+  `sessionID` varchar(6) NOT NULL,
+  `username` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Questions`
+--
+ALTER TABLE `Questions`
+  ADD PRIMARY KEY (`qID`);
+
+--
+-- Indexes for table `Sessions`
+--
+ALTER TABLE `Sessions`
+  ADD PRIMARY KEY (`sessionID`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `userfav`
+--
+ALTER TABLE `userfav`
+  ADD KEY `sessionID` (`sessionID`,`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Questions`
+--
+ALTER TABLE `Questions`
+  MODIFY `qID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `User`
+--
+ALTER TABLE `User`
+  ADD CONSTRAINT `User_ibfk_1` FOREIGN KEY (`username`) REFERENCES `Sessions` (`sessionID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
